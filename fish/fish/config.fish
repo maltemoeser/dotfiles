f0ee8078991f50ae99803c4199982e6c25ff4149
Malte Möser <ilovegit@maltemoeser.de>
2016-08-17 14:13:34 +0200

    Disable command-not-found.

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -16,9 +16,6 @@ eval (thefuck --alias | tr '\n' ';')
 # Disable Homebrew Analytics
 set -gx HOMEBREW_NO_ANALYTICS 1
 
-# Homebrew's command not found
-brew command command-not-found-init > /dev/null; and . (brew command-not-found-init)
-
 # Improve Bluetooth Audio
 defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 45
 
